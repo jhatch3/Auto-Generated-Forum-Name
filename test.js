a8fb744ae107cc57150a04968b8be7b13c8f854f
Justin hatch <115518370+jhatch3@users.noreply.github.com>
2024-08-05 15:19:47 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-# Logic For Saving Forum to Folder - Cited (https://www.youtube.com/watch?v=4zoL-lIukb4)
+# Logic For Saving Forum to Folder 
+# Cited (https://www.youtube.com/watch?v=4zoL-lIukb4)
 
 mySaveAs = app.trustPropagatorFunction(function(doc,path) {app.beginPriv(); doc.saveAs(path); app.endPriv(); })
 myTrustedSpecialTaskFunc = app.trustedFunction(function(doc,path) {app.beginPriv(); mySaveAs(doc,path); app.endPriv(); });
